22183c2be1e4207d1101687505a1016d5eaa61a9
NotBitcoinCEO <96089214+NotBitcoinCEO@users.noreply.github.com>
2022-04-30 11:53:31 -0700

    Added new page

---
+++ lesson2/index.js
@@ -0,0 +1,11 @@
+// Hicks law
+// Fitts law
+// White space and clean design
+
+// INCLUDE:
+// •	the design principle's title,
+// •	the organization/company name
+// • 	the web site address as a external hyperlink,
+// •	a mobile view screenshot* of an organization's web page that, in your opinion, illustrates that principle, and
+// •	a brief description on how the design principle is exemplified on that page.
+
